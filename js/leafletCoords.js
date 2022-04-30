c4df51b0e2104e0b48e296f528845c07b9662a49
Francisnei-ZaptTech <francisnei@zapt.tech>
2022-04-30 15:02:02 -0300

    Adjust structure

--- js/leafletCoords.js
+++ js/leafletCoords.js
@@ -4,10 +4,11 @@ let markers = [];
 let map = null;
 let route = null;
 let imageOverlay = null;
+let dashArray = '1'
+
 function getMaxBounds(){
   return L.latLngBounds(L.latLng(imageObj.height, imageObj.width), L.latLng(0,0));
 }
-
 function initializeMap(){
   map = L.map('map', {
     crs: L.CRS.Simple,
@@ -31,7 +32,6 @@ function initializeMap(){
     console.log(pathsRoute);
   });
 }
-
 function changeMap(){
   map.setMaxBounds(getMaxBounds());
   map.setZoom(map.getBoundsZoom(getMaxBounds(), false));
@@ -41,7 +41,6 @@ function changeMap(){
   updateMarkers();
   updateRoute();
 }
-
 function updateMarkers(){
   markers.map(marker => marker.remove());
   pathsRoute.map((coords, index)=>{
@@ -53,24 +52,20 @@ function updateMarkers(){
     );
   });
 }
-
 function updatePointCoords(index, event){
   let newCoords = event.target && event.target._latlng;
   pathsRoute[index] = [newCoords.lat, newCoords.lng];
   updateRoute();
 }
-
 function deletePoint(index, event){
   pathsRoute.splice(index, 1);
   updateMarkers();
   updateRoute();
 }
-
 function updateRoute(){
   route.setLatLngs(pathsRoute);
   route.redraw();
 }
-
 function exportPaths(){
   leafletCoords = pathsRoute.map(path => transformCoordinatesToLeaflet(path));
   let string = `${
@@ -92,7 +87,6 @@ function copyPathsToClipBoard(){
 
   navigator.clipboard.writeText(string);
 }
-
 function transformCoordinatesToLeaflet(coords){
   let y = imageObj.height - Math.round(coords[0]);
   let x = Math.round(coords[1]);
@@ -100,8 +94,7 @@ function transformCoordinatesToLeaflet(coords){
   return result;
 }
 
-
-const inputImage = document.getElementById('image-input');
+const inputImage = document.getElementById('image-map');
 inputImage.addEventListener('change', (e)=>{
   let file = e.target.files[0];
   const reader = new FileReader();
@@ -113,4 +106,34 @@ inputImage.addEventListener('change', (e)=>{
     changeMap()
   }
   imageObj.src = URL.createObjectURL(file);
-})
+});
+
+const buttonOpenSetting = document.getElementById('button-drawer-settings');
+buttonOpenSetting.addEventListener('click', (e)=>{
+  const drawerSetting = document.getElementById('drawer-settings');
+  const navBar = document.getElementById('nav-bar');
+  navBar.toggleAttribute('transform');
+  drawerSetting.toggleAttribute('open');
+});
+
+const selectStyleRoutes = document.getElementById('select-style');
+const dropDownContent = document.getElementById('drop-down-styles');
+selectStyleRoutes.addEventListener('click',(e)=>{
+  dropDownContent.toggleAttribute('show');
+});
+
+const styleItems = document.getElementsByClassName('style-item');
+if(styleItems.length > 0){
+  for(item of styleItems){
+    item.addEventListener('click', onChangeStyleRoute.bind(this, item))
+  }
+}
+
+function onChangeStyleRoute(item, e){
+  if(e.target.tagName == 'svg'){
+    dashArray = item.getAttribute('stroke-dasharray');
+    selectStyleRoutes.children[0].remove();
+    selectStyleRoutes.appendChild(e.target.cloneNode(true));
+    dropDownContent.toggleAttribute('show');
+  }
+}
